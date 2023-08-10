e6d4abdf69b161b6fd5dce8b0fbbbcdee37c408b
Torh-21 <majarotoheeb@gmail.com>
2023-08-09 18:05:23 -0700

    The 1-last_digit.py program is added

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_dig = abs(number) % 10
+msg = "Last digit of"
+if last_dig < 0:
+    last_dig = -last_dig
+print("{} {} is {} and is ".format(msg, number, last_dig), end="")
+if last_dig > 5:
+    print("greater than 5")
+elif last_dig == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -1,2 +1,3 @@
 This is the README file for the 0x01-python-if_else_loops_functions directory.
 The 0-positive_or_negative.py program assigns a random signed number to the variable number each time it is executed.
+The 1-last_digit.py program assigns a  random signed number to the variable number each time it is executed.
