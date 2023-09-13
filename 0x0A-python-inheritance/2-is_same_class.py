8b8348745561c3732caa9268b0555322c4c73009
Torh-21 <majarotoheeb@gmail.com>
2023-09-13 15:12:25 -0700

    task 2 added

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+    This module defines the prototype def is_same_class(obj, a_class):
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+        This function returns true if obj is
+        the exact class a_class, otherwise false
+    """
+    return (type(obj) == a_class)
