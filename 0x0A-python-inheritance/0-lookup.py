7f53ba85e6a48d7e79abaf93857f3cb777107180
Torh-21 <majarotoheeb@gmail.com>
2023-09-13 14:56:16 -0700

    formatting fix for lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 
-""" 
+"""
     This module defines the prototype def lookup(obj):
 """
 
 
 def lookup(obj):
-    """ 
-        This function returns a list of available 
-        attributes and methods of an object. 
+    """
+        This function returns a list of available
+        attributes and methods of an object.
     """
     return dir(obj)
