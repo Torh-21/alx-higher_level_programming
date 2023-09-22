d2a43bde2d8a7923858f5b09645f5dd6c1212764
Torh-21 <majarotoheeb@gmail.com>
2023-09-22 13:54:21 -0700

    Hot fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,7 +15,7 @@ class Rectangle(Base):
         which inherits from the Base class
     """
     
-    def __init_(self, width, height, x=0, y=0, id=None):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """
             This method initializes a new Rectangle
             Args:
@@ -56,3 +56,65 @@ class Rectangle(Base):
         else:
             self.__width = value
 
+    @property
+    def height(self):
+        """
+            returns the height of the Rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+            sets the height of the Rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
+
+    @property
+    def x(self):
+        """
+            returns the x-coordinate of the Rectangle
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+            sets the x-coordinate of the Rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
+
+    @property
+    def y(self):
+        """
+            sets the y-coordinate of the Rectangle
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+            sets the y-coordinate of the Rectangle
+        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
+
+    def area(self):
+        """
+            returns the area of the Rectangle
+        """
+        return self.width * self.height
