402c5b7f76f887e6bf05eeb3cfc0f729a34f5a66
Torh-21 <majarotoheeb@gmail.com>
2023-12-01 13:56:46 -0800

    Python Network #1 task started

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This python script fetches the link: https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+url = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content:".format(html.decode("utf-8", "replace")))
