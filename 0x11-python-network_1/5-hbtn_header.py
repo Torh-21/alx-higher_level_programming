2e794b7f451d78f3d11312ebe5fa3b9ac37959ad
Torh-21 <majarotoheeb@gmail.com>
2023-12-01 15:35:42 -0800

    Formatting error fix

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """This python script takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header"""
+
+
 import requests
 import sys
+
 
 if __name__ == "__main__":
     req = requests.get(sys.argv[1])
