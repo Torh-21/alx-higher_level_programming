f4b89edba09887e072b5752cd2bb7aa46658e56b
Torh-21 <majarotoheeb@gmail.com>
2023-12-05 03:45:50 -0800

    Task 10 added

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This python script takes 2 arguments in order to list 10 commits (from the most recent to oldest) of a repository"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(sys.argv[2], sys.argv[1]))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        for com in r.json()[:10]:
+            print("{}: {}".format(com.get('sha'),
+                                  com.get('commit').get('author').get('name')))
